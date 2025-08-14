40bdb5843b69195ed8ee1877316a65f4337c3eaa
Leandro Arrigonni <lean.arrigonni@gmail.com>
2025-08-13 21:32:29 -0300

    feat: :sparkles: Primsa Module Integration

---
+++ src/infrastructure/prisma/prisma.module.ts
@@ -0,0 +1,42 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { PrismaCustomerRepository } from './repositories/customer.repository';
+import { PrismaCommerceRepository } from './repositories/commerce.repository';
+import { PrismaActivityLogRepository } from './repositories/activityLog.repository';
+import { PrismaServicesRepository } from './repositories/services.repository';
+import { PrismaBookingRepository } from './repositories/booking.repository';
+
+@Global()
+@Module({
+  providers: [
+    PrismaService,
+    {
+      provide: 'ICustomerRepository',
+      useClass: PrismaCustomerRepository,
+    },
+    {
+      provide: 'ICommerceRepository',
+      useClass: PrismaCommerceRepository,
+    },
+    {
+      provide: 'IActivityLogRepository',
+      useClass: PrismaActivityLogRepository,
+    },
+    {
+      provide: 'IServicesRepository',
+      useClass: PrismaServicesRepository,
+    },
+    {
+      provide: 'IBookingRepository',
+      useClass: PrismaBookingRepository,
+    },
+  ],
+  exports: [
+    'ICustomerRepository',
+    'ICommerceRepository',
+    'IActivityLogRepository',
+    'IServicesRepository',
+    'IBookingRepository',
+  ],
+})
+export class PrismaModule {}
