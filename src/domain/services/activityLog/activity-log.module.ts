f5a892e121e4f179da9764a1c8d78d5920b5abf3
Leandro Arrigonni <lean.arrigonni@gmail.com>
2025-08-21 00:54:28 -0300

    feat: :sparkles: Activity Log is now a global module

--- src/domain/services/activityLog/activity-log.module.ts
+++ src/domain/services/activityLog/activity-log.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ActivityLogService } from './activity-log.service';
 import { PrismaService } from '@/infrastructure/prisma/prisma.service';
 import { PrismaActivityLogRepository } from '@/infrastructure/prisma/repositories/activityLog.repository';
 import { ACTIVITY_LOG_REPOSITORY } from '@/application/constants/providers';
 
+@Global()
 @Module({
   providers: [
     ActivityLogService,
