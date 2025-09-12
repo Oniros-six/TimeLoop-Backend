75d5e5006dcb40f27189c88cc2753044f8b835e9
Leandro Arrigonni <lean.arrigonni@gmail.com>
2025-09-12 17:28:01 -0300

    feat: :sparkles: CORS Enable

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 import * as passport from 'passport';
 import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cors({
+    origin: ['http://localhost:4321', 'http://localhost:3000', 'http://www.timeloop.com.uy'], // Puerto de Astro
+    credentials: true
+  }))
+  
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
