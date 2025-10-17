8192f790db9c1911c88520aceba3d9249ddf98f8
Leandro Arrigonni <lean.arrigonni@gmail.com>
2025-10-16 21:30:29 -0300

    chore: :bug: Make optional service module update info

--- src/interfaces/controllers/services/dto/update-service.dto.ts
+++ src/interfaces/controllers/services/dto/update-service.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsNumber, IsString, MaxLength, Min, MinLength } from 'class-validator';
+import { IsNumber, IsOptional, IsString, MaxLength, Min, MinLength } from 'class-validator';
 
 export class UpdateServiceDto {
+  @ApiProperty({ example: 1, description: 'ID del servicio' })
+  @Type(() => Number)
+  @IsNumber({}, { message: 'El ID del servicio debe ser un número' })
+  id: number;
+
   @ApiProperty({ example: 1, description: 'ID del comercio' })
   @Type(() => Number)
   @IsNumber({}, { message: 'El ID del comercio debe ser un número' })
@@ -17,6 +22,7 @@ export class UpdateServiceDto {
   userId: number;
 
   @ApiProperty({ example: 'Corte de pelo', description: 'Nombre del servicio' })
+  @IsOptional()
   @IsString({ message: 'El nombre tiene que contener solo letras' })
   @MinLength(3, { message: 'El nombre debe tener al menos 3 caracteres' })
   name?: string;
@@ -25,6 +31,7 @@ export class UpdateServiceDto {
     example: 'Corte de cabello tradicional adaptado a tu estilo. Incluye lavado, corte con tijera y máquina, retoque de contornos y peinado con producto de terminación. Ideal para quienes buscan un look prolijo y fresco.',
     description: 'Descripción del servicio'
   })
+  @IsOptional()
   @IsString({ message: 'La descripción tiene que contener solo letras' })
   @MaxLength(500, { message: 'La descripción debe contenter maximo 500 caracteres' })
   description?: string;
@@ -33,6 +40,7 @@ export class UpdateServiceDto {
     example: '200',
     description: 'Costo del servicio',
   })
+  @IsOptional()
   @Type(() => Number)
   @IsNumber({}, { message: 'El precio debe ser un número' })
   @Min(100, { message: 'El precio debe tener al menos 3 dígitos' })
@@ -42,6 +50,7 @@ export class UpdateServiceDto {
     example: '20',
     description: 'Duración del servicio en minutos',
   })
+  @IsOptional()
   @Type(() => Number)
   @IsNumber({}, { message: 'La duración debe ser un número' })
   @Min(10, { message: 'La duración debe ser mas de 10 minutos' })
